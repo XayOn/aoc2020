2b7b98a8a18c70ea418bbdbe640a78e69ceb7042
David Francos Cuartero <me@davidfrancos.net>
2020-12-03 21:23:06 +0100

    Even less code

--- second.py
+++ second.py
@@ -1,25 +1,12 @@
-"""Day 2
-
-First part: python3.8 first.py input_file 0 # First policy
-Second part: python3.8 first.py input_file 1 # Second policy
-"""
-
-import re
-import sys
+"""Day 2"""
+import re, sys
 from collections import Counter
 
-PARSE_LINE = re.compile('(\d+)-(\d+) (\w): (.*)')
-POLICIES = [
-    lambda m, x, l, s: m <= Counter(s)[l] <= x,
-    lambda m, x, l, s: Counter(a == l for a in (s[m - 1], s[x - 1]))[True] == 1
+LNS = re.compile('(\d+)-(\d+) (\w): (.*)')
+IN = lambda x: int(x) - 1
+POLS = [
+    lambda m, x, l, s: int(m) <= Counter(s)[l] <= int(x),
+    lambda m, x, l, s: Counter(a == l for a in (s[IN(m)], s[IN(x)]))[True] == 1
 ]
-
-
-def get_valid_passwords(policy, entries):
-    for line in entries:
-        min_, max_, letter, string = PARSE_LINE.match(line).groups()
-        yield policy(int(min_), int(max_), letter, string)
-
-
-lines = open(sys.argv[1]).readlines()
-print(Counter(get_valid_passwords(POLICIES[int(sys.argv[2])], lines))[True])
+res = open(sys.argv[1]).readlines()
+print(len([l for l in res if POLS[int(sys.argv[2])](*LNS.match(l).groups())]))
