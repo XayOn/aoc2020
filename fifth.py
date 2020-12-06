c3bec037832205a5ae6fdc3e5b5f93a6fa2c1d1c
David Francos Cuartero <me@davidfrancos.net>
2020-12-06 01:56:23 +0100

    Using frozenset

--- fifth.py
+++ fifth.py
@@ -4,9 +4,9 @@ https://github.com/alejandroscf/advent-20/blob/main/05/1.py
 import sys, functools
 
 M = {'F': 0, 'B': 1, 'L': 0, 'R': 1}
-results = list(
+results = frozenset(
     int(functools.reduce(lambda x, y: f'{M.get(x, x)}{M.get(y, y)}', r), 2)
     for r in open(sys.argv[1]).readlines())
-empty = set(range(1024)).difference(results)
+empty = frozenset(range(1024)).difference(results)
 print(max(results))
 print(next(a for a in empty if a + 1 in results and a - 1 not in empty))
